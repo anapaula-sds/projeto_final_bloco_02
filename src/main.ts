60a529d50197cb8a892b76363e0b6836cbc7faed
Ana Paula <anapaula-sds@hotmail.com>
2025-01-28 13:15:43 -0300

    :sparkles: Construção do Projeto e Conexão com o Banco de dados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  process.env.TZ = '-03:00'
+  //Habilitar globalmente a validação de dados
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Habilitar a CORS na aplicação
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
